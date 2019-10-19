0cf79e8e86796217ae2b38c59ea29f8fff65da2b
Ricky dildy <rdildy92@gmail.com>
2019-10-19 01:01:00 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -2,6 +2,8 @@ describe (using_times) do
   it "puts out the phrase'Wingardium Leviosa' 7 times buy using the 'times' construct" do 
     
     looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
+    expect{using_times}.to ouput(looping_string).to_stdout
+  end 
 end
 
 
