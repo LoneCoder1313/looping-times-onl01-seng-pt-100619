19c2bc609292278a56929bf5c1ffcba149e3c835
Ricky dildy <rdildy92@gmail.com>
2019-10-19 01:42:59 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,9 +1,8 @@
-describe (using_times) do 
-  it "puts out the phrase'Wingardium Leviosa' 7.times"
-  7.times do 
+ (using_times) = "puts out the phrase'Wingardium Leviosa'"
+def 7.times 
     	puts (using_times)
   end 
-end
+
 
 
 
